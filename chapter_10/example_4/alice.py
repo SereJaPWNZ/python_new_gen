0c10a36d83a87bc1d8748eb95af8b94f9fb91a68
SereJaPWNZ <oofatherxomgoo@gmail.com>
2024-01-17 21:58:31 +0300

    add app

--- chapter_10/example_4/alice.py
+++ chapter_10/example_4/alice.py
@@ -1,12 +1,19 @@
-filename = "alice.txt"
+def count_words(filename):
+    """Подсчет приблизительного количества строк в файле."""
+    try:
+        with open(filename, encoding="utf-8") as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Данного файла нет в данной директории!")
+    else:
+        #
+        words = contents.split()
+        nuw_words = len(words)
+        print(f"{filename} {nuw_words}")
 
-try:
-    with open(filename, encoding="utf-8") as f:
-        contents = f.read()
-except FileNotFoundError:
-    print(f"Данного файла нет в данной директории!")
-else:
-    #
-    words = contents.split()
-    nuw_words = len(words)
-    print(f"{filename} {nuw_words}")
+
+filenames = ['alice.txt', 'siddhartha.txt', 'moby_dick.txt',
+             'little_women.txt']
+
+for filename in filenames:
+    count_words(filename)
