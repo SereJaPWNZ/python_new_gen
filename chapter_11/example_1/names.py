162ef0d9190fcc43aefcf688dfe652156c323250
SereJaPWNZ <oofatherxomgoo@gmail.com>
2024-01-21 21:38:08 +0300

    написана программа по выводу отформатированного имени

--- chapter_11/example_1/names.py
+++ chapter_11/example_1/names.py
@@ -1,2 +1,13 @@
 from name_function import get_formatted_name
 
+print("\nВведите 'q' для выхода.")
+while True:
+    first = input("Введите ваше имя:\n")
+    if first.lower() == "q" or first.lower() == "й":
+        break
+    last = input("Введите вашу фамилию:\n")
+    if last.lower() == "q" or last.lower() == "й":
+        break
+
+    formatted_name = get_formatted_name(first, last)
+    print(f"\n\tNeatly formatted name: {formatted_name}.")
