f06874cc6176b7cadfeea040ab1db4d62170b392
SereJaPWNZ <oofatherxomgoo@gmail.com>
2024-01-25 19:21:15 +0300

    программа запрашивающая название города и страну

--- chapter_11/11.1. city_country/cities.py
+++ chapter_11/11.1. city_country/cities.py
@@ -1,2 +1,14 @@
 from city_functions import get_formatted_city
 
+print(f"Введите 'q' для выхода")
+
+while True:
+    city = input("Введите название города:\n")
+    if city == 'q' or city == '':
+        break
+    country = input("Введите название страны:\n")
+    if country == 'q' or country == '':
+        break
+
+    formatted_city = get_formatted_city("Volgograd", "Russia")
+    print(formatted_city)
