4e0ee2888da70f377429c74de9f4b60699f47c3c
SereJaPWNZ <oofatherxomgoo@gmail.com>
2024-02-02 21:52:54 +0300

    update

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,35 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры."""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы."""
+        pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_weight,
+                                               self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            # Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # При каждом проходе цикла перерисовывается экран
+            self.screen.fill(self.settings.bg_color)
+
+            # Отображение последнего прорисованного экрана
+            pygame.display.flip()
+
+
+if __name__ == "__main__":
+    ai = AlienInvasion()
+    ai.run_game()
