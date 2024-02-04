3d9840003e34eccf093aae9bc73dc6298f57cd6c
SereJaPWNZ <oofatherxomgoo@gmail.com>
2024-02-04 12:36:36 +0300

    update f-string

--- chapter_6/many_users.py
+++ chapter_6/many_users.py
@@ -14,7 +14,7 @@ users = {
 
 for username, user_info in users.items():
     print(f'\nUsername: {username}')
-    full_name = f'{user_info['first']} {user_info['last']}'
+    full_name = f'{user_info["first"]} {user_info["first"]}'
     location = user_info['location']
     print(f"\tFull name: {full_name.title()}")
     print(f"\tLocation: {location.title()}")
